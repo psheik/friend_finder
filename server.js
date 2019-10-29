9762246db75e93103f17af320a926c4bd210bca9
Pasha Sheik <pashasheik33@gmail.com>
2019-10-29 02:28:13 -0700

    added screen shots and sql parameters

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var friends = path.basename('/Users/Refsnes/friends.js');
+var friends = path.basename("../db/friends.js");
 console.log(friends);
 var app = express();
 var PORT = process.env.PORT || 8080;
-app.use(express.static(__dirname + "/app/css"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
@@ -15,3 +14,13 @@ require("./app/routing/htmlRoutes.js")(app);
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
 });
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 8080,
+	user: 'root',
+	password: '',
+	database: 'friend_finder_db'
+});
+console.log(connection);
